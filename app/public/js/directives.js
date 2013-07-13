3bf3abbd06a3ac5fe1ea8adc18ed9a44c66a58f0
akashkrishnan <ak@aakay.net>
2013-07-13 14:02:04 -0700

    Added ngEnter directive.

--- app/public/js/directives.js
+++ app/public/js/directives.js
@@ -2,9 +2,20 @@
 
 /* Directives */
 
-angular.module('myApp.directives', []).
-  directive('appVersion', function (version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+angular.module('myApp.directives', []).directive('appVersion', function (version) {
+	return function(scope, elm, attrs) {
+		elm.text(version);
+	};
+}).directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
     };
-  });
+});;
